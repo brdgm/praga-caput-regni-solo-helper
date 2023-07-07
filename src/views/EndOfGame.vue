e7d07927c7bd16c21a13b546df6cabb979798a12
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-07-07 09:01:33 +0200

    back button on game end screen

--- src/views/EndOfGame.vue
+++ src/views/EndOfGame.vue
@@ -4,13 +4,16 @@
   <p class="mt-4" v-html="t('endOfGame.info1')"></p>
   <p v-html="t('endOfGame.info2')"></p>
 
-  <FooterButtons endGameButtonType="endGame"/>
+  <FooterButtons :backButtonRouteTo="backButtonRouteTo" endGameButtonType="endGame"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import FooterButtons from '@/components/structure/FooterButtons.vue'
+import NavigationState from '@/util/NavigationState'
+import { useRoute } from 'vue-router'
+import { useStateStore } from '@/store/state'
 
 export default defineComponent({
   name: 'EndOfGame',
@@ -19,7 +22,16 @@ export default defineComponent({
   },
   setup() {
     const { t } = useI18n()
-    return { t }
+    const route = useRoute()
+    const state = useStateStore()
+    const navigationState = new NavigationState(route, state)
+    const { botCount } = navigationState
+    return { t, botCount }
+  },
+  computed: {
+    backButtonRouteTo() : string {
+      return `/round/16/bot/${this.botCount}`
+    }
   }
 })
 </script>
