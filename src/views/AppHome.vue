e7558e56441034e785703de01cf182127d3b5e0d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-11-19 20:54:25 +0100

    fix code smells

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -2,7 +2,7 @@
 
   <div class="float-end m-2 w-25">
     <a href="https://boardgamegeek.com/boardgame/308765/praga-caput-regni" target="_blank" rel="noopener">
-      <img src="../assets/game-praga-caput-regni.jpg" class="rounded shadow w-100"/>
+      <img src="../assets/game-praga-caput-regni.jpg" class="rounded shadow w-100" alt=""/>
     </a>
   </div>
 
@@ -20,7 +20,7 @@
 
   <ModalDialog id="peterParlerModal" title="Peter Parler" :size-lg="true">
     <template #body>
-      <img src="@/assets/peter-parler.jpg"/>
+      <img src="@/assets/peter-parler.jpg" alt=""/>
       <p v-html="t('home.peterParler1')"></p>
       <p v-html="t('home.peterParler2')"></p>
     </template>
@@ -32,7 +32,7 @@
     This is not an official Delicious Games product and has no affiliation with <a href="https://deliciousgames.org/" target="_blank" rel="noopener">Delicious Games</a>.
     All art in this app is from <a href="https://deliciousgames.org/praga-caput-regni-en" target="_blank" rel="noopener">Praga Caput Regni</a>.
     See <a href="#" data-bs-toggle="modal" data-bs-target="#creditsModal">{{t('footer.credits')}}</a>.
-    <a href="https://ko-fi.com/brdgm_me" target="_blank" rel="noopener"><img class="kofi-button" src="@/assets/kofi_button_dark.png"/></a>
+    <a href="https://ko-fi.com/brdgm_me" target="_blank" rel="noopener"><img class="kofi-button" src="@/assets/kofi_button_dark.png" alt="Support me on Ko-fi"/></a>
   </p>
 
 </template>
